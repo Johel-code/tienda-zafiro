7131200c268f104eb609b2aa2bce1a30a262d6e8
unknown <camila>
2023-04-03 12:48:44 -0400

    botones-Guardar-Productos-Cancelar

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,6 +5,23 @@
         </h2>
     </x-slot>
 
+<div> 
+<head>
+  <link href="https://cdn.tailwindcss.com" rel="stylesheet">
+</head>
+
+<body>
+  <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+    Guardar
+  </button>
+  <button class="bg-[#597AAB] hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">
+    Cancelar
+  </button>
+</body>
+</div>
+
+
+    
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
--- resources/views/livewire/crear-products.blade.php
+++ resources/views/livewire/crear-products.blade.php
@@ -1,3 +1,15 @@
 <div>
-    {{-- Because she competes with no one, no one can compete with her. --}}
+<head>
+  <link href="https://cdn.tailwindcss.com" rel="stylesheet">
+</head>
+
+<body>
+  <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+    Guardar
+  </button>
+  <button class="bg-[#597AAB] hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">
+    Cancelar
+  </button>
+</body>
 </div>
+
