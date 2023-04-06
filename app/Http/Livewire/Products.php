5ab3f67859b9a8a45c90541fc354be226c3b1c17
unknown <bojorgeaestefani@gmail.com>
2023-04-05 22:50:38 -0400

    crear buscar producto

--- app/Http/Livewire/Products.php
+++ app/Http/Livewire/Products.php
@@ -3,11 +3,19 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
-
+use App\Models\User;
 class Products extends Component
 {
+    public $searchTerm = "";
+    public $products;
     public function render()
     {
+        if(emputy($this ->searchTerm)){
+            $this-> products = User::where('name',$this->searchTerm)->get();
+
+        }else{
+            $this->products= User::where('name','like','%'.$this->searchTerm.'%')->get ();
+        }
         return view('livewire.products');
     }
 }
