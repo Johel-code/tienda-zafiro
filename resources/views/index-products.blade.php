2b9dfc70b6b75094d965a7378e0435a763b6cc23
JuanDazaS <jadaza7832@gmail.com>
2023-04-05 22:13:28 -0400

    correcciones de las vistas

--- resources/views/index-products.blade.php
+++ resources/views/index-products.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.web')
 
 @section('content')
-<div class="mb-4 bg-[#E3E9F1] px-10 pl-48 pb-8 mb-4 ml-20 pt-2 mt-4">
+<div class="bg-[#E3E9F1] px-10 pl-48 pb-8 ml-24 pt-2 mt-4">
     <h2 class="text-4xl font-bold mb-2">PRODUCTOS</h2>
 </div>
 <div class="flex ml-10">
--- resources/views/livewire/products.blade.php
+++ resources/views/livewire/products.blade.php
@@ -21,10 +21,10 @@
                         @foreach($products as $product)
                         <tr>
                             <th class="text-center font-normal">{{$product->id}}</th>
-                            <th class="text-center font-normal">{{$product->name_product}}</th>
+                            <th class="text-left font-normal">{{$product->name_product}}</th>
                             <th class="text-center font-normal">{{$product->precio}}</th>
                             <th class="text-center font-normal">{{$product->cantidad_inventario}}</th>
-                            <th class="text-center font-normal">{{$product->fecha_vencimiento}}</th>
+                            <th class="text-left font-normal">{{$product->fecha_vencimiento}}</th>
                             <th class="text-center font-normal">{{$product->marca}}</th>
                             <th class="text-center font-normal">{{$product->category_id}}</th>
                             <td class="text-center">
@@ -45,26 +45,6 @@
                             </td>
                         </tr>
                         @endforeach
-                        <tr>
-                            <td class="text-center">2</td>
-                            <td class="text-center">2</td>
-                            <td class="text-center">ola</td>
-                            <td class="text-center">25</td>
-                            <td class="text-center">10</td>
-                            <td class="text-center">unilever</td>
-                            <td class="text-center">12-04-26</td>
-                            <td class="text-center">
-                                <label class="relative inline-flex items-center cursor-pointer items-center">
-                                    <input type="checkbox" value="" class="sr-only peer">
-                                    <div class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-3 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-                                </label>
-                            </td>
-                            <td class="text-center"><a href="dashboard"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-28 h-6">
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
-                                    </svg>
-                                </a>
-                            </td>
-                        </tr>
                     </tbody>
                 </table>
             </div>
