5fe482a45615d247d9a86ff5a6ac2735fe11d82f
JuanDazaS <jadaza7832@gmail.com>
2023-04-06 20:21:29 -0400

    cambios para el deploy

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -25,32 +25,23 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            /*'name' => $this->faker->name(),
+
+            'name' => $this->faker->name,
+            'last_name' => $this->faker->lastName,
+            'ci' => $this->faker->numberBetween(10000000, 99999999),
+            'direccion' => $this->faker->sentence(),
+            'celular' => $this->faker->numberBetween(60000000, 80000000),
+            'genero' => $this->faker->randomElement(['M', 'F']),
+            'fecha_nacimiento' => $this->faker->date(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'two_factor_secret' => null,
-            'two_factor_recovery_codes' => null,
-            'remember_token' => Str::random(10),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+            'activo_user' => $this->faker->boolean(),
+
+            'role_id' => $this->faker->numberBetween(1, 10),
+
+            'current_team_id' => null,
             'profile_photo_path' => null,
-            'current_team_id' => null,*/
-
-            'name'=> $this->faker->name,
-            'last_name'=>$this->faker->lastName,
-            'ci'=>$this->faker->numberBetween(10000000,99999999),
-            'direccion'=>$this->faker->sentence(),
-            'celular'=>$this->faker->numberBetween(60000000,80000000),
-            'genero'=>$this->faker->randomElement(['M','F']),
-            'fecha_nacimiento'=>$this->faker->date(),
-            'email'=>$this->faker->unique()->safeEmail(),
-            'email_verified_at'=> now(),
-            'password'=> '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
-            'activo_user'=>$this->faker->boolean(),
-
-            'role_id'=>$this->faker->numberBetween(1,10),
-
-            'current_team_id'=> null,
-            'profile_photo_path'=> null,
         ];
     }
 
@@ -71,14 +62,14 @@ class UserFactory extends Factory
      */
     public function withPersonalTeam(): static
     {
-        if (! Features::hasTeamFeatures()) {
+        if (!Features::hasTeamFeatures()) {
             return $this->state([]);
         }
 
         return $this->has(
             Team::factory()
                 ->state(function (array $attributes, User $user) {
-                    return ['name' => $user->name.'\'s Team', 'user_id' => $user->id, 'personal_team' => true];
+                    return ['name' => $user->name . '\'s Team', 'user_id' => $user->id, 'personal_team' => true];
                 }),
             'ownedTeams'
         );
