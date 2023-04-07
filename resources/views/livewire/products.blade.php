9d934acadc2fea13fa4d308e8668d843f8ec25f7
Johel Saavedra <jbsch16@gmail.com>
2023-04-07 11:54:01 -0400

    toggle button vinculado dinamicamente con base de datos

--- resources/views/livewire/products.blade.php
+++ resources/views/livewire/products.blade.php
@@ -28,15 +28,8 @@
                             <th class="text-center font-normal">{{$product->marca}}</th>
                             <th class="text-center font-normal">{{$product->category_id}}</th>
                             <td class="text-center">
-                                <label class="relative inline-flex items-center cursor-pointer items-center">
-                                    @if($product->estado_product)
-                                    <input type="checkbox" value="" class="sr-only peer" checked wire:click="editEstado({{$product->id}},{{$product->estado_product}})">
-                                    @else(
-                                    <input type="checkbox" value="" class="sr-only peer" wire:click="editEstado({{$product->id}},{{$product->estado_produt}})">
-                                    )
-                                    @endif
-                                    <div class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-3 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-                                </label>
+                                <livewire:product.toggle-product :product="$product" :field="'estado_product'"
+                                :key="'toggle-button'.$product->id">
                             </td>
                             {{-- <td class="text-center">
                                 <a href="dashboard">
