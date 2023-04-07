ba4f0577c8dc134cffbb73d29d0bcfbbcfbab017
katherine ortiz <katherineortiz440ggmail.com>
2023-04-07 19:36:25 -0400

    agregado de buscador

--- resources/views/livewire/crear-products.blade.php
+++ resources/views/livewire/crear-products.blade.php
@@ -1,11 +1,5 @@
-    <div id="ventana_registrar_productos" class="flex justify-center items-center h-4/5  ml-0    right-0 mt-10 ">
-        <div class="bg-[#E3E9F1] px-10 pl-48 ml-20">
-    <div class="relative w-80 ml-96 left-96 pl-40">
-            <input type="search"  class=" p-2.5 w-80 z-20 text-sm text-gray-900 bg-gray-50 rounded-lg border-l-gray-300 border border-gray-300 focus:ring-gray-300 focus:border-gray-300" placeholder="Buscar">
-            <button type="submit" class="absolute top-0 left-96 ml-14 p-2.5 bg-gray-50 rounded-r-lg border border-gray-300 hover:bg-gray-200 focus:ring-2 focus:outline-none focus:ring-gray-300">
-                <svg class="w-5 h-5" fill="none" stroke="black" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
-            </button>
-        </div>
+    <div id="ventana_registrar_productos" class="flex justify-center items-center h-4/5  ml-0    right-0 mt-10 ">   
+    <div class="bg-[#E3E9F1] px-10 pl-48 ml-20">
             <form class="px-20 mx-20 mb-10 ">
                 <div class="mb-4 grid grid-cols-4 gap-4">
                     <div class="">
