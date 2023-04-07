489b45282d5ce9ec16f17487681000dd00357957
JuanDazaS <jadaza7832@gmail.com>
2023-04-06 22:45:30 -0400

    mayuscula asset

--- public/index.php
+++ public/index.php
@@ -31,7 +31,7 @@ if (file_exists($maintenance = __DIR__ . '/../storage/framework/maintenance.php'
 |
 */
 
-require __DIR__ . '/../../TIENDA-ZAFIRO/vendor/autoload.php';
+require __DIR__ . '/../../tienda-zafiro/vendor/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -44,7 +44,7 @@ require __DIR__ . '/../../TIENDA-ZAFIRO/vendor/autoload.php';
 |
 */
 
-$app = require_once __DIR__ . '/../../TIENDA-ZAFIRO/bootstrap/app.php';
+$app = require_once __DIR__ . '/../../tienda-zafiro/bootstrap/app.php';
 
 $kernel = $app->make(Kernel::class);
 
