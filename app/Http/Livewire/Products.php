2c3fa26c0847cc8de6117fa79c35f4299d2b36e3
Johel Saavedra <jbsch16@gmail.com>
2023-04-08 15:41:00 -0400

    buscador funcionando como mayus y minus

--- app/Http/Livewire/Products.php
+++ app/Http/Livewire/Products.php
@@ -10,6 +10,10 @@ use Livewire\Component;
 class Products extends Component
 {
     use WithPagination;
+    
+    public $searchTerm = "";
+    //public $products;
+
 
     public function updatingSearch()
     {
@@ -18,8 +22,12 @@ class Products extends Component
 
     public function render()
     {
+        $products = Product::when($this->searchTerm, function($query, $searchTerm){
+            return $query->whereRaw('LOWER(name_product) LIKE ? ', ['%'.trim(strtolower($searchTerm)).'%']);
+        });
+        $products = $products->paginate(10);
         return view('livewire.products', [
-            'products' => Product::paginate(10),
+            'products' => $products
         ]);
     }
 }
