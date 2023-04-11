472b618c1f6285b809d47202b33f199181b6b4d3
Belen <belugonzalez.203@gmail.com>
2023-04-11 03:45:02 -0400

    crear-prod-responsive

--- resources/views/livewire/crear-products.blade.php
+++ resources/views/livewire/crear-products.blade.php
@@ -1,13 +1,13 @@
-<div id="ventana_registrar_productos" class="flex justify-center">
+<div id="ventana_registrar_productos" class="flex justify-center items center h-4/5 ">
     
-    <div class="bg-[#E3E9F1] mx-2 my-5">
+    <div class="bg-[#E3E9F1] mx-2 my-2 md:ml-28 sm:ml-24 sm:mr-0">
         @if(session()->has('message'))
             <div class="bg-green-100 border border-green-400 text-green-700 px-2 py-3 rounded relative" role="alert">
                 <strong class="font-bold">{{ session('message') }}</strong>
             </div>
         @endif
-        <form wire:submit.prevent="submit" class="px-10 mx-1 my-2 ">
-            <div class="mb-4 grid grid-cols-4 gap-4">
+        <form wire:submit.prevent="submit" class="px-10 mx-1 my-2 lg:ml-60 md:px-6">
+            <div class="mb-4 grid grid-cols-4">
                 <div class="">
                     <label class="block text-black-700 text-lg font-bold mx-0 w-9" for="nombre">
                         Nombre:
@@ -19,7 +19,7 @@
                     @error('nombre') <span class="error">{{ $message }}</span> @enderror
                 </div>
             </div>
-            <div class="mb-4 grid grid-cols-4 gap-4">
+            <div class="mb-4 grid grid-cols-4">
                 <div class="">
                     <label class="block text-black-700 text-lg font-bold mx-0 w-10" for="descripcion">
                         Descripción:
@@ -32,7 +32,7 @@
                 </div>
             </div>
             <div class="grid grid-cols-2 gap-4">
-                <div class="mb-4 grid grid-cols-2 gap-4">
+                <div class="mb-4 grid grid-cols-2 gap-2">
                     <div class="">
                         <label class="block text-black-700 text-lg font-bold mx-0 w-10" for="cantidad">
                             Cantidad:
@@ -43,23 +43,23 @@
                         id="cantidad" type="number" placeholder="Cantidad del producto" wire:model="cantidad">
                     </div>
                 </div>
-                <div class="mb-4 grid grid-cols-4 gap-4">
-                    <div class="">
+                <div class="mb-4 grid grid-cols-4 gap-4 lg:gap-16 md:gap-20 sm:gap-0">
+                    <div class="sm:col-span-2">
                         <label class="block text-black-700 text-lg font-bold mx-0 w-10" for="precio">
                             Precio:
                         </label>
                     </div>
-                    <div class="col-span-3">
+                    <div class="col-span-2">
                         <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 border-solid border-black leading-tight focus:outline-none focus:shadow-none bg-[#E3E9F1]" 
                         id="precio" type="number" step="0.01" placeholder="Precio del producto" wire:model="precio">
                     </div>
                 </div>
             </div>
             <div class="grid grid-cols-2 gap-4">
-                <div class="mb-4 grid grid-cols-2 gap-4">
+                <div class="mb-4 grid grid-cols-2 gap-2">
                     <div class="">
                         <label class="block text-black-700 text-lg font-bold" for="cantidad minima">
-                            Cantidad Mínima:
+                            Cant Mínima:
                         </label>
                     </div>
                     <div class="">
@@ -67,14 +67,14 @@
                         id="cantidad minima" type="number" placeholder="Cantidad min. producto" wire:model="cantidad_minima">
                     </div>
                 </div>
-                <div class="mb-4 grid grid-cols-4 gap-4">
-                    <div class="">
+                <div class="mb-4 grid grid-cols-4 gap-4 lg:gap-16 md:gap-20 sm:gap-0">
+                    <div class="sm:col-span-2">
                         <label 
                             class="block text-black-700 text-lg font-bold " for="categoria">
                                 Categoría:
                         </label>
                     </div>
-                    <div class="col-span-3">
+                    <div class="col-span-3 sm:col-span-2">
                         <select id="categoria_id" wire:model="categoria" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 border-solid border-black leading-tight focus:outline-none focus:shadow-none bg-[#E3E9F1] ">
                             <option value="">Seleccione una categ.</option>
                             @foreach($categorias as $categoria)
@@ -86,7 +86,7 @@
             </div>
 
             <div class="grid grid-cols-2 gap-4">
-                <div class="mb-4 grid grid-cols-2 gap-4">
+                <div class="mb-4 grid grid-cols-2 gap-2">
                     <div class="">
                         <label class="block text-black-700 text-lg font-bold mr-1" for="costo adquisicion">
                             Costo Adquisición:
@@ -94,16 +94,16 @@
                     </div>
                     <div class="">
                         <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 border-solid border-black leading-tight focus:outline-none focus:shadow-none bg-[#E3E9F1] " 
-                        id="costo Adquisicion" type="number" step="0.01" placeholder="Costo Adquisición" wire:model="adquisicion">
+                        id="costo Adquisicion" type="number" step="0.01" placeholder="Costo Adquisición" sm:placeholder="Costo" wire:model="adquisicion">
                     </div>
                 </div>
-                <div class="mb-4 grid grid-cols-4 gap-4">
-                    <div class="">
+                <div class="mb-4 grid grid-cols-4 gap-4 lg:gap-16 md:gap-20 sm:gap-0">
+                    <div class="sm:col-span-2">
                         <label class="block text-black-700 text-lg font-bold mx-0 w-10" for="marca">
                             Marca:
                         </label>
                     </div>
-                    <div class="col-span-3">
+                    <div class="col-span-3 sm:col-span-2">
                         <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 border-solid border-black leading-tight focus:outline-none focus:shadow-none bg-[#E3E9F1]" 
                         id="marca" type="text" placeholder="Marca del producto" wire:model="marca">
                     </div>
@@ -112,7 +112,7 @@
 
             <div class="grid grid-cols-2 gap-4">
           
-                <div class="mb-4 grid grid-cols-4 gap-4">
+                <div class="mb-4 grid grid-cols-4 gap-2">
                     <div class="col-span-2">
                         <label class="block text-black-700 text-lg font-bold mx-0 w-10" for="proveedor">
                             Proveedor:
@@ -128,13 +128,13 @@
                     </div>
                 </div>
 
-                <div class="mb-4 grid grid-cols-4 gap-4">
-                    <div class="">
+                <div class="mb-4 grid grid-cols-4 gap-4 lg:gap-16 md:gap-20 sm:gap-0">
+                    <div class="sm:col-span-2">
                         <label class="block text-black-700 text-lg font-bold mx-0 w-10" for="fecha">
                             Fecha:
                         </label>
                     </div>
-                    <div class="col-span-3">
+                    <div class="col-span-3 sm:col-span-2">
                         <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-500 border-solid border-black leading-tight focus:outline-none focus:shadow-none bg-[#E3E9F1] " 
                         id="fecha" type="date" wire:model="fecha">
                     </div>
@@ -143,29 +143,29 @@
 
             <div class="grid grid-cols-2 gap-4">
                 
-                <div class="mb-4 grid grid-cols-5 gap-4">
-                    <div class="">
+                <div class="mb-4 grid grid-cols-4 gap-2">
+                    <div class="col-span-2">
                         <label class="block text-black-700 text-lg font-bold mx-0 w-10" for="foto">
                             Foto:
                         </label>
                     </div>
                     
-                    <div class="col-span-4">
-                        <input  class=" appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-none bg-[#E3E9F1] " 
-                        id="foto" type="file" wire:model="foto">
+                    <div class="col-span-2">
+                        <input  class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-500 border-solid border-black leading-tight focus:outline-none focus:shadow-none bg-[#E3E9F1] " 
+                        id="foto" type="file" wire:model="foto" >
                         @if ($foto)
                             <img src="{{ $foto->temporaryUrl() }}">
                         @endif
                     </div>
 
                 </div>
-                <div class="mb-4 grid grid-cols-4 gap-4">
-                    <div class="">
+                <div class="mb-4 grid grid-cols-4 gap-4 lg:gap-16 md:gap-20 sm:gap-0">
+                    <div class="sm:col-span-2">
                         <label class="block text-black-700 text-lg font-bold mx-0 w-10" for="estado">
                             Estado:
                         </label>
                     </div>
-                    <div class="col-span-3">
+                    <div class="col-span-3 sm:col-span-2">
                         <label class="relative inline-flex items-center mb-4 cursor-pointer">
                             <input checked type="checkbox" value="" class="sr-only peer" wire:model="estado">
                             <div class="w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus: ring-blue-800 dark:peer-focus:ring-blue-800 dark:bg-gray-500 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600">
