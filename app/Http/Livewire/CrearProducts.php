65b77fee6b71448a1b904c98f89fa5145008cc15
JuanDazaS <jadaza7832@gmail.com>
2023-04-11 22:18:51 -0400

    implementacion del modal

--- app/Http/Livewire/CrearProducts.php
+++ app/Http/Livewire/CrearProducts.php
@@ -12,14 +12,15 @@ class CrearProducts extends Component
 {
     use WithFileUploads;
     public $nombre, $descripcion, $cantidad, $precio, $marca,
-            $categoria, $fecha, $estado, $foto,$id_product, 
-            $proveedor,$cantidad_minima, $adquisicion,$categorias, $proveedors;
+        $categoria, $fecha, $estado, $foto, $id_product,
+        $proveedor, $cantidad_minima, $adquisicion, $categorias, $proveedors;
 
     protected $rules = [
-        'nombre' => 'required|max:4',
+        'nombre' => 'required|max:20',
         'foto' => 'required|image|max:1024',
-        'cantidad' => 'required',
-        'precio' => 'required',
+        'cantidad' => 'required|number',
+        'cantidad_minima' => 'required|number',
+        'precio' => 'required|number',
         'marca' => 'required',
         'categoria' => 'required',
         'descripcion' => 'required'
@@ -30,11 +31,12 @@ class CrearProducts extends Component
     ];
 
     public function render()
-    {   
+    {
         return view('livewire.crear-products');
     }
 
-    public function limpiar(){
+    public function limpiar()
+    {
         $this->nombre = '';
         $this->cantidad = '';
         $this->precio = '';
@@ -56,7 +58,7 @@ class CrearProducts extends Component
         $this->proveedors = Provider::all();
     }
     public function save()
-{
+    {
         $this->validate([
             'foto' => 'image|max:1024', // validar que se cargó una imagen y que es menor a 1MB
         ]);
@@ -65,27 +67,28 @@ class CrearProducts extends Component
             $path = $this->foto->storePublicly('public/images');
             $this->foto = str_replace('public', 'storage', $path);
         }
-}
+    }
     public function submit()
-    {   
+    {
         $this->validate();
-        Product::updateOrCreate(['id'=>$this->id_product],
+        Product::updateOrCreate(
+            ['id' => $this->id_product],
             [
-                'codigo' =>random_int(10000, 99999),
+                'codigo' => random_int(10000, 99999),
                 'name_product' => $this->nombre,
                 'descripcion' => $this->descripcion,
                 'cantidad_inventario' => $this->cantidad,
-                'cantidad_minima'=> $this->cantidad_minima,
-                'costo_adquisicion'=> $this->adquisicion,
+                'cantidad_minima' => $this->cantidad_minima,
+                'costo_adquisicion' => $this->adquisicion,
                 'precio' => $this->precio,
                 'marca' => $this->marca,
-                'category_id' => $this->categoria, 
+                'category_id' => $this->categoria,
                 'fecha_vencimiento' => $this->fecha,
-                'provider_id' => $this->proveedor, 
+                'provider_id' => $this->proveedor,
                 'image' => $this->foto ? url($this->foto) : 'default_image.jpg',
             ]
         );
-        session()->flash('message','¡Producto añadido exitosamente!');
+        session()->flash('message', '¡Producto añadido exitosamente!');
         $this->limpiar();
     }
 }
