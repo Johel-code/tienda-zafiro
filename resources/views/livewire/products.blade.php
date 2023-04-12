a5d300ac7f5c7c2eda3286e02c080a52b06712db
yoMarks <cristianheredia789@gmail.com>
2023-04-12 09:56:56 -0400

    barAdmin-mejorposicioon-conBuscador

--- resources/views/livewire/products.blade.php
+++ resources/views/livewire/products.blade.php
@@ -1,15 +1,15 @@
 <div>
     <div>
         <div class="flex flex-row place-content-between">
-            <div class="flex lg:ml-72 md:ml-72 sm:ml-8">
+            <div class=" lg:ml-72 md:ml-72 sm:ml-8">
                 <a href="/crear-products">
                     <button class=" bg-[#3988FF] hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                         AÑADIR
                     </button>
                 </a>
             </div>
-            <div class="flex sm:px-8 lg:px-8 md:ml-64">
-                <input wire:model="search" type="search"  class=" p-2.5 w-80 z-20 text-sm text-gray-900 bg-gray-50 rounded-lg border-l-gray-300 border border-gray-300 focus:ring-gray-300 focus:border-gray-300" placeholder="Buscar">
+            <div class=" lg:mr-8 md:mr-8 sm:mr-8">
+                <input wire:model="search" type="search"  class=" p-2.5 w-auto sm:w-80 text-sm text-gray-900 bg-gray-50 rounded-lg border-l-gray-300 border border-gray-300 focus:ring-gray-300 focus:border-gray-300" placeholder="Buscar">
                 {{-- <button type="submit" class="absolute top-0 left-96 ml-20 pl-4 p-2.5 bg-gray-50 rounded-r-lg border border-gray-300 hover:bg-gray-200 focus:ring-2 focus:outline-none focus:ring-gray-300">
                     <svg class="w-5 h-5" fill="none" stroke="black" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
                 </button> --}}
