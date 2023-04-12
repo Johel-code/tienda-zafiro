ee24e077f77b8f1837e6b0b96b5b99a8e2effeac
Johel Saavedra <jbsch16@gmail.com>
2023-04-12 10:41:13 -0400

    validacion con espacios y imgae

--- app/Http/Livewire/CrearProducts.php
+++ app/Http/Livewire/CrearProducts.php
@@ -16,17 +16,17 @@ class CrearProducts extends Component
         $proveedor, $cantidad_minima, $adquisicion, $categorias, $proveedors;
 
     protected $rules = [
-        'nombre' => 'required|max:15|alpha_num',
-        //'foto' => 'required|image|max:1024|mimes:jpg,jpeg,png',
+        'nombre' => 'required|max:15|regex:/^[a-zA-Z0-9. ]+$/',
+        'foto' => 'required|image|max:1024|mimes:jpg,jpeg,png',
         'cantidad' => 'required|numeric|min:1 ',
         'precio' => 'required|numeric |min:1 ',
-        'marca' => 'required|max:10| alpha_num',
+        'marca' => 'required|max:10|regex:/^[a-zA-Z0-9 ]+$/',
         'cantidad_minima' => 'required|numeric|min:1',
         'adquisicion' => 'required|numeric|min:1',
         'categoria' => 'required',
         'proveedor'  => 'required',
         'descripcion' => 'max:50',
-        'fecha' => 'after:tomorrow|nullable'
+        'fecha' => 'after:today|nullable'
     ];
 
     protected $messages = [
@@ -38,21 +38,21 @@ class CrearProducts extends Component
         'cantidad_minima.numeric' => 'Solo se admiten números enteros',
         'cantidad_minima.min' => 'No se admiten números negativos',
         'nombre.required' => 'El campo nombre es requerido',
-        'nombre.alpha_num' => 'El campo nombre solo puede tener letras y números',
+        'nombre.regex' => 'El campo nombre solo puede tener letras y números',
         'nombre.max' => 'El nombre no debe tener más de 15 caracteres',
         'descripcion.max' =>'Solo se admiten 50 caracteres',
         'cantidad.required' => 'El campo cantidad es requerido',
         'cantidad.numeric' => 'Solo se admiten números enteros',
         'cantidad.min' => 'No se admiten números negativos',
         'estado.required' => 'El campo estado es requerido',
-        // 'foto.required' => 'El campo foto es requerido',
-        // 'foto.max' => 'Solo se permite como maximo 1024',
-        // 'foto.mimes' => 'Solo se admite una imagen o foto',
+        'foto.required' => 'El campo foto es requerido',
+        'foto.max' => 'Solo se permite como maximo 1024',
+        'foto.image' => 'Solo se admite una imagen o foto',
         'precio.required' => 'El campo precio es requerido',
         'precio.min' => 'El valor minimo a ingresar es 1',
         'marca.required' => 'El campo marca es requerido',
         'marca.max' => 'Solo se admiten máximo 10 caracteres',
-        'marca.alpha_num' => 'Solo puede ingresar letras y números',
+        'marca.regex' => 'Solo puede ingresar letras y números',
         'categoria.required' => 'El campo categoria es requerido',
         'fecha.after' => 'Debe ingresar una fecha posterior a la actual'
     ];
@@ -84,20 +84,27 @@ class CrearProducts extends Component
         $this->categorias = Category::all();
         $this->proveedors = Provider::all();
     }
-    public function save()
-    {
-        $this->validate([
-            'foto' => 'image|max:1024', // validar que se cargó una imagen y que es menor a 1MB
-        ]);
 
+
+    // public function save()
+    // {
+    //     // $this->validate([
+    //     //     'foto' => 'image|max:1024', // validar que se cargó una imagen y que es menor a 1MB
+    //     // ]);
+
+    //     if ($this->foto) {
+    //         $path = $this->foto->storePublicly('public/images');
+    //         $this->foto = str_replace('public', 'storage', $path);
+    //         dd($this);
+    //     }
+    // }
+    public function submit()
+    {
+        $this->validate();
         if ($this->foto) {
             $path = $this->foto->storePublicly('public/images');
             $this->foto = str_replace('public', 'storage', $path);
         }
-    }
-    public function submit()
-    {
-        $this->validate();
         Product::updateOrCreate(
             ['id' => $this->id_product],
             [
