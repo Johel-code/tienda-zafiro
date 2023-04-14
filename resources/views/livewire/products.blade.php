35e547af12667e83d8575f2e8c456df0028c5971
Belen <belugonzalez.203@gmail.com>
2023-04-14 17:03:54 -0400

    guardado antes de pull

--- resources/views/livewire/products.blade.php
+++ resources/views/livewire/products.blade.php
@@ -9,7 +9,7 @@
                 </a>
             </div>
             <div class="flex sm:px-8 lg:px-8 md:ml-48 ">
-                <input wire:model="search" type="search"  class=" p-2.5 w-80 md:w-56 z-20 text-sm text-gray-900 bg-gray-50 rounded-lg border-l-gray-300 border border-gray-300 focus:ring-gray-300 focus:border-gray-300" placeholder="Buscar">
+                <input wire:model="search" type="search"  class=" p-2.5 w-80 md:w-56 z-20 text-sm text-gray-900 bg-gray-50 rounded-lg z-0 border-l-gray-300 border border-gray-300 focus:ring-gray-300 focus:border-gray-300" placeholder="Buscar">
                 {{-- <button type="submit" class="absolute top-0 left-96 ml-20 pl-4 p-2.5 bg-gray-50 rounded-r-lg border border-gray-300 hover:bg-gray-200 focus:ring-2 focus:outline-none focus:ring-gray-300">
                     <svg class="w-5 h-5" fill="none" stroke="black" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
                 </button> --}}
@@ -22,7 +22,7 @@
         <div class="py-2"></div>        
 
         <div class="sm:px-8 lg:px-8 md:ml-32">
-            <div class="bg-white  overflow-hidden md:overflow-x-auto shadow-x1 sm:rounded-lg px-4 py-4 md:shrink">
+            <div class="bg-white  overflow-hidden md:overflow-x-auto shadow-x1  sm:rounded-lg px-4 py-4 md:shrink">
                 <table class="md:table-fixed w-full">
                     <thead class="md:py-8">
                         <tr class="bg-white text-dark">
