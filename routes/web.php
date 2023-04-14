6a9fad3300606b86212e3ab604bb12fed14b25bf
Johel Saavedra <jbsch16@gmail.com>
2023-04-13 22:24:21 -0400

    Revert "ruta eliminada"
    This reverts commit 6e474e02193eeda51735890bead3603d7952bda7.

--- routes/web.php
+++ routes/web.php
@@ -34,3 +34,8 @@ Route::middleware(['auth:sanctum', 'verified'])->group(function (){
     //Route::view('/products', 'index-products');
     Route::view('/crear-products', 'index-create-products');
 });
+
+Route::get('/create-symlink', function (){
+    symlink(storage_path('/app/public'), public_path('storage'));
+    echo "Symlink Created. Thanks";
+});
