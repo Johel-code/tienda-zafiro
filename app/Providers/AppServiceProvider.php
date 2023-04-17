4da63f57bfd47c5c392ba55923dc0f9264ce6b39
Johel Saavedra <jbsch16@gmail.com>
2023-04-17 13:55:19 -0400

    quitando service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,8 +19,5 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if ($this->app->environment('production')) {
-            \Illuminate\Support\Facades\URL::forceScheme('https');
-        }
     }
 }
