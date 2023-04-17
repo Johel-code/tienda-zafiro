60d5f9cec1da9c1a406af5d3575573b9eee6fb80
Johel Saavedra <jbsch16@gmail.com>
2023-04-16 22:07:01 -0400

    prueba 3 solucion image

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import laravel, { refreshPaths } from 'laravel-vite-plugin';
 import fs from 'fs'; 
  
-const host = 'tienda-zafiro-production.up.railway.app'; 
+const host = 'my-app.test'; 
 
 
 export default defineConfig({
