0ffcb8d7890a7064b459438b0fc52a0aecbbb8ee
JuanDazaS <jadaza7832@gmail.com>
2023-04-27 23:33:56 -0400

    acomodo de tabla

--- resources/views/livewire/products.blade.php
+++ resources/views/livewire/products.blade.php
@@ -1,7 +1,7 @@
 <div>
     <div>
         <div class="flex flex-row place-content-between">
-            <div class="flex lg:ml-16 md:ml-8 sm:ml-8">
+            <div class="flex lg:ml-20 md:ml-8 sm:ml-8">
                 <a href="/crear-products">
                     <button class=" bg-[#3988FF] hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                         AÑADIR
@@ -9,7 +9,7 @@
                 </a>
             </div>
 
-            <div class="flex sm:px-2 lg:px-8 lg:ml-48 md:ml-24 sm:ml-8">
+            <div class="flex sm:px-2 lg:px-12 lg:ml-48 md:ml-24 sm:ml-8">
                 <input wire:model="search" type="search"  class=" p-2.5 w-80 md:w-56 text-sm text-gray-900 bg-gray-50 rounded-lg border-l-gray-300 border border-gray-300 focus:ring-gray-300 focus:border-gray-300" placeholder="Buscar">
                 {{-- <button type="submit" class="absolute top-0 left-96 ml-20 pl-4 p-2.5 bg-gray-50 rounded-r-lg border border-gray-300 hover:bg-gray-200 focus:ring-2 focus:outline-none focus:ring-gray-300">
                     <svg class="w-5 h-5" fill="none" stroke="black" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
@@ -22,7 +22,7 @@
         
         <div class="py-2"></div>        
 
-        <div class="sm:px-8 lg:px-8 md:ml-8">
+        <div class="sm:px-8 lg:px-12 md:ml-8">
             <div class="bg-white  overflow-hidden overflow-x-auto shadow-x1 sm:rounded-lg px-4 py-2 shrink"style="padding-left:2px;padding-right: 2px;">
                 <table class="md:table-fixed w-full font-anek">
                     <thead class="md:py-8">
