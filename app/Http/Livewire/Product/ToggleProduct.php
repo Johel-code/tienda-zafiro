02dd95c981976a9c6f8cfc516976d155727cf037
JuanDazaS <jadaza7832@gmail.com>
2023-04-28 10:18:54 -0400

    correcion de rutas

--- app/Http/Livewire/Product/ToggleProduct.php
+++ app/Http/Livewire/Product/ToggleProduct.php
@@ -11,17 +11,20 @@ class ToggleProduct extends Component
     public Product $product;
     public bool $isActive;
     public string $field;
-    
+
     public $mostrarModalSwitch = false;
-    public function abrirModalSwitch(){
+    public function abrirModalSwitch()
+    {
         $this->mostrarModalSwitch = true;
     }
-    public function cerrarModalSwitch(){
-        $this->updating($this->field,true);
+    public function cerrarModalSwitch()
+    {
+        $this->updating($this->field, true);
         redirect('/');
     }
-    public function confirmarSwitch(){
-        $this->updating($this->field,false);
+    public function confirmarSwitch()
+    {
+        $this->updating($this->field, false);
         redirect('/');
     }
 
@@ -36,14 +39,14 @@ class ToggleProduct extends Component
 
     public function render()
     {
-        return view('livewire.vendedor.product.toggle-product');
+        return view('livewire.product.toggle-product');
     }
 
     public function updating($field, $value)
     {
-        if($this->isActive){
+        if ($this->isActive) {
             $this->abrirModalSwitch();
-        }else{
+        } else {
             $this->product->setAttribute($this->field, $value)->save();
             $this->emit('refresh');
         }
