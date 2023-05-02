83aab2d71beb2b14d1935e297a339ce86b28aa9c
yoMarks <cristianheredia789@gmail.com>
2023-05-02 11:05:35 -0400

    aviso del switchProducto cambiado

--- resources/views/livewire/product/toggle-product.blade.php
+++ resources/views/livewire/product/toggle-product.blade.php
@@ -9,20 +9,25 @@
         <div class="flex items-center justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
             <div class="modal-bg-container fixed inset-0 bg-gray-700 bg-opacity-30"></div>
             <div class="modal-space-container hidden sm:inline-block sm:align-middle sm:h-screen"></div>
-            <div class="border-8 border-white inline-block bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all align-middle" style="width: 9cm; height: 3.5cm;">
+            <div class="border-4 border-white inline-block bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all align-middle" style="width: 9.5cm; height: 4.3cm;">
             <!--Contenido-->
-
-                <div class="flex justify-center mb-5 mt-6">
-                    <h2 class=" text-xl font-bold">¿Estás seguro?</h2>
+                <div class="flex justify-center mb-0 mt-2">
+                    <h2 class=" text-2xl font-semibold ">Este producto sera dado de baja </h2>
+                </div>
+                <div class="flex justify-center mb-0 mt-0">
+                    <h2 class=" text-xl font-medium">Producto: {{$product->name_product}}</h2>
+                </div>
+                <div class="flex justify-center mb-0 mt-0">
+                    <h2 class=" text-2xl font-semibold">¿Estás seguro?</h2>
                 </div>
 
                 <div class="flex justify-center">
                     
                     <button type=button wire:click="confirmarSwitch()" class="rounded-md border border-white shadow-md 
-                         bg-[#3988FF] hover:bg-blue-700 font-bold text-white py-1 mr-6  w-28">SI
+                         bg-[#3988FF] hover:bg-blue-700 font-bold text-white py-1 mt-1 mr-6 w-28">SI
                     </button>
                     <button type=button wire:click="cerrarModalSwitch()" class="rounded-md border border-white shadow-md 
-                        bg-[#597AAB]  hover:bg-gray-700 font-bold text-white py-1 ml-6 w-28">NO
+                        bg-[#597AAB]  hover:bg-gray-700 font-bold text-white py-1 mt-1 ml-6 w-28">NO
                     </button>
                 </div>
     
--- resources/views/livewire/product/toggle-product2.blade.php
+++ resources/views/livewire/product/toggle-product2.blade.php
@@ -9,12 +9,17 @@
         <div class="flex items-center justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
             <div class="modal-bg-container fixed inset-0 bg-gray-700 bg-opacity-30"></div>
             <div class="modal-space-container hidden sm:inline-block sm:align-middle sm:h-screen"></div>
-            <div class="border-8 border-white inline-block bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all align-middle" style="width: 9cm; height: 3.5cm;">
-            <!--Contenido-->
-
-                <div class="flex justify-center mb-5 mt-6">
-                    <h2 class=" text-xl font-bold">¿Estás seguro?</h2>
-                </div>
+            <div class="border-4 border-white inline-block bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all align-middle" style="width: 9.5cm; height: 4.3cm;">
+                <!--Contenido-->
+                    <div class="flex justify-center mb-0 mt-2">
+                        <h2 class=" text-2xl font-semibold ">Este producto sera dado de alta<a href="http://lta" target="_blank" rel="noopener noreferrer"></a> </h2>
+                    </div>
+                    <div class="flex justify-center mb-0 mt-0">
+                        <h2 class=" text-xl font-medium">Producto: {{$product->name_product}}</h2>
+                    </div>
+                    <div class="flex justify-center mb-0 mt-0">
+                        <h2 class=" text-2xl font-semibold">¿Estás seguro?</h2>
+                    </div>
 
                 <div class="flex justify-center">
                     
