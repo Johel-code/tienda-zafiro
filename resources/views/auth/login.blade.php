cf5071517fceee0b76d314f1a80468156ec15667
yoMarks <cristianheredia789@gmail.com>
2023-05-14 16:56:46 -0400

    fondo login y zafiro

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,27 +1,40 @@
 <x-guest-layout>
-    <div class="block mx-auto my-12 p-8 bg-white w-1/3 border border-gray-200 rounded-lg shadow-lg">
+    <div style="background-image: url('logo/login_fondo.png'); background-size: cover; background-position: center;"
+         class="fondo-login min-h-screen flex flex-col justify-center items-center">
         
-        <h1 class="text-3xl text-center font-bold">Login</h1>
-
-        <form class="mt-4" method="POST" action="">
-            @csrf
-
-            <input type="email" class="border border-gray-200 rounded-md bg-gray-200 w-full text-lg
-            placeholder-gray-900 p-2 my-2 focus:bg-white" placeholder="Email" id="email" name="email">
-
-            <div class="relative">
-                <input type="password" class="border border-gray-200 rounded-md bg-gray-200 w-full text-lg placeholder-gray-900 p-2 my-2 focus:bg-white" placeholder="Password" id="password" name="password">
-                <img src="logo/eyeoculto.png" alt="" class="hover:border-white hover:border rounded-full cursor-pointer absolute translate-y-1/2 top-1 right-3 w-7 opacity-30" id="eye">
+         <div class="modal-login w-8/12 sm:max-w-md  my-6 px-14 py-4 bg-white shadow-lg overflow-hidden rounded-lg">
+            
+            <div class="text-center mt-5 mb-5">
+                <span class="text-3xl font-anek font-bold text-[#063780]">INICIAR SESION</span>
             </div>
+            
+            <form method="POST" action="">
+                @csrf
 
-            @error('message')
-            <p class="border border-red-500 rounded-md bg-red-100 w-full text-red-600 p-2 my-2">* {{ $message }}</p>
-            @enderror
+                
+                <input type="email" class="border border-gray-200 rounded-md bg-gray-200 w-full text-lg
+                placeholder-gray-900 p-2 my-2 focus:bg-white" id="email" name="email">
 
-            <button type="submit" class=" rounded-md bg-indigo-500 w-full text-lg text-white font-semibold
-            p-2 my-3 hover:bg-indigo-600">Send</button>
+                <div class="relative">
+                    <input type="password" class="border border-gray-200 rounded-md bg-gray-200 w-full text-lg placeholder-gray-900 p-2 my-2 focus:bg-white" placeholder="Password" id="password" name="password">
+                    <img src="logo/eyeoculto.png" alt="" class="hover:border-white hover:border rounded-full cursor-pointer absolute translate-y-1/2 top-1 right-3 w-7 opacity-30" id="eye">
+                </div>
 
-        </form>
+                @error('message')
+                <p class="border border-red-500 rounded-md bg-red-100 w-full text-red-600 p-2 my-2">* {{ $message }}</p>
+                @enderror
+
+                <button type="submit" class=" rounded-md bg-indigo-500 w-full text-lg text-white font-semibold
+                p-2 my-3 hover:bg-indigo-600">Send</button>
+            </form>
+
+        </div>
+
+        <div style="position: fixed; margin-top: 9rem; margin-right: -53rem; width: 27rem;" class="hidden sm:block">
+            <div class="self-end">
+                <img src="{{ asset('logo/zafiro.png') }}" alt="zafiro" class="">
+            </div>
+        </div>
 
         <script>
             var eye= document.getElementById('eye');
@@ -38,7 +51,6 @@
                 }
             })
         </script>
-          
-</div>
-    
+
+    </div>    
 </x-guest-layout>
