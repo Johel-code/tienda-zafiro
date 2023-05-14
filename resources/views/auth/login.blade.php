b464b0015b4e0234f3de0920ce2002d2780892bf
yoMarks <cristianheredia789@gmail.com>
2023-05-14 05:32:30 -0400

    iconos ocultara y visualisar password

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,8 +9,10 @@
             <input type="email" class="border border-gray-200 rounded-md bg-gray-200 w-full text-lg
             placeholder-gray-900 p-2 my-2 focus:bg-white" placeholder="Email" id="email" name="email">
 
-            <input type="password" class="border border-gray-200 rounded-md bg-gray-200 w-full text-lg
-            placeholder-gray-900 p-2 my-2 focus:bg-white" placeholder="Password" id="password" name="password">
+            <div class="relative">
+                <input type="password" class="border border-gray-200 rounded-md bg-gray-200 w-full text-lg placeholder-gray-900 p-2 my-2 focus:bg-white" placeholder="Password" id="password" name="password">
+                <img src="logo/eyeoculto.png" alt="" class="hover:border-white hover:border rounded-full cursor-pointer absolute translate-y-1/2 top-1 right-3 w-7 opacity-30" id="eye">
+            </div>
 
             @error('message')
             <p class="border border-red-500 rounded-md bg-red-100 w-full text-red-600 p-2 my-2">* {{ $message }}</p>
@@ -21,5 +23,22 @@
 
         </form>
 
-    </div>
+        <script>
+            var eye= document.getElementById('eye');
+            var password= document.getElementById('password');
+            eye.addEventListener("click",function(){
+                if(password.type == "password"){
+                    password.type = "text"
+                    eye.style.opacity=0.7
+                    eye.src= "logo/eyevisible.png"
+                }else{
+                    password.type = "password"
+                    eye.style.opacity=0.3
+                    eye.src= "logo/eyeoculto.png"
+                }
+            })
+        </script>
+          
+</div>
+    
 </x-guest-layout>
