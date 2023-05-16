ebec746691fc2297cbfd8482c0ea9df4a311b41c
Belen <belugonzalez.203@gmail.com>
2023-05-16 19:08:05 -0400

    Botones-guardar-cancelar

--- resources/views/livewire/ventas.blade.php
+++ resources/views/livewire/ventas.blade.php
@@ -1,3 +1,31 @@
-<div>
-    {{-- Close your eyes. Count to one. That is how long forever feels. --}}
+<div id="ventas-pre-factura" class="flex justify-center items center ">
+    <div>
+        <div>
+            <div id="buscador">
+
+            </div>
+
+            <div id="cantidad">
+
+            </div>
+
+            <div id="boton-añadir">
+
+            </div>
+        </div>
+
+        <div id="canasta">
+
+        </div>
+
+        <div id="botones-guardar-cancelar">
+            <button type="submit" class="bg-[#3988FF] hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                GUARDAR
+            </button>
+            <button type=button wire:click="$emit('show-modalConfirmacionVendedor')" class=" ml-32 bg-[#597AAB] hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">
+                CANCELAR
+            </button>
+        </div>
+    </div>
+    
 </div>
