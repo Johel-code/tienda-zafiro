e56c22d400e089d9bfa9ad7c2a4113c7bab65eaf
Johel-code <201910122@est.umss.edu>
2023-05-19 17:08:39 -0400

    primer paso para factura

--- app/Http/Livewire/EmitirFactura.php
+++ app/Http/Livewire/EmitirFactura.php
@@ -29,4 +29,48 @@ class EmitirFactura extends Component
         //dd($this->datos);
         return redirect()->to('/pre-factura');
     }
+
+    public function submit()
+    {
+        $this->validate();
+        $factura = new Invoice;
+        
+        $factura->save();
+        generarPDF();
+    }
+
+    public function generarPDF()
+    {
+        $vista = view('factura', [
+            'codigoFactura' => $this->codigoFactura,
+            'ciNit' => $this->ciNit,
+            'nombreCliente' => $this->nombreCliente,
+            'productos' => $this->productos,
+            'total' => $this->total,
+        ])->render();
+
+        // Crear una nueva instancia de Dompdf
+        $dompdf = new Dompdf();
+
+        $dompdf->setPaper('letter');
+
+        // Cargar el contenido HTML en Dompdf
+        $dompdf->loadHtml($vista);
+
+        // Renderizar el PDF
+        $dompdf->render();
+
+        // Obtener el contenido del PDF como una cadena
+        $contenidoPDF = $dompdf->output();
+
+        // Generar una respuesta HTTP con el contenido del PDF
+        $response = new \Illuminate\Http\Response($contenidoPDF);
+
+        // Establecer las cabeceras para indicar que es un archivo PDF
+        $response->header('Content-Type', 'application/pdf');
+        $response->header('Content-Disposition', 'attachment; filename="factura.pdf"');
+
+        // Devolver la respuesta HTTP
+        return $response;
+    }
 }
