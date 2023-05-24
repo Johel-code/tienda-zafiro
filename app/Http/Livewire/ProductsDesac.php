54da533bc4ed69d250c23dc6146887f8ce87bcdb
JuanDazaS <jadaza7832@gmail.com>
2023-05-24 17:53:52 -0400

    productos inactivos terminado

--- app/Http/Livewire/ProductsDesac.php
+++ app/Http/Livewire/ProductsDesac.php
@@ -10,21 +10,34 @@ use Livewire\WithPagination;
 class ProductsDesac extends Component
 {
     use WithPagination;
+    public $search = "";
+    //public $products;
+    public $category;
+
+    public function updatingSearch()
+    {
+        $this->resetPage();
+    }
+
     public function render()
     {
-        $category = Category::all();
-        $productsDesac = Product::where('estado_product', 0)
-            ->orderBy('name_product', 'asc')->paginate(5);
+        $this->category = Category::all();
+        $productsDesac = Product::where('estado_product', 0)->when($this->search, function ($query, $search) {
+            return $query->whereRaw('LOWER(name_product) LIKE ? ', ['%' . trim(strtolower($search)) . '%'])
+                ->orwhere('codigo', 'LIKE', '%' . $this->search . '%');
+        });
 
+
+        $productsDesac = $productsDesac->orderBy('name_product', 'asc')->paginate(5);
         //$this->emit('refrescarModal');
 
+
         return view('livewire.products-desac', [
-            'productsDesac' => $productsDesac,
-            'category' => $category
+            'productsDesac' => $productsDesac
         ]);
         //return view('livewire.products-desac');
     }
-    
+
     public function mostrarModal($productId)
     {
         $this->emit('mostrarModal', $productId);
